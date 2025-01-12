3361a876b8e36d419584b8986cd7b72b9e6c49f8
Usuario <Usuario@DESKTOP-FNJ8BDH>
2025-01-12 23:08:26 +0100

    ns que es esto

---
+++ Tarea15-Johann/src/main/Controlador.java
@@ -0,0 +1,20 @@
+package main;
+
+import dao.AlumnoDao;
+import dao.AlumnoDaoImpl;
+import vista.IVista;
+import vista.VistaConsola;
+
+public class Controlador {
+
+	public static void main(String[] args) {
+//		AlumnoDao modelo = new AlumnoDaoImpl();
+//		IVista vista = new VistaConsola();
+//		new Controlador().ejecutar(modelo, vista);
+
+	}
+
+	public void ejecutar(AlumnoDao modelo, IVista vista) {
+		// TODO
+	}
+}
